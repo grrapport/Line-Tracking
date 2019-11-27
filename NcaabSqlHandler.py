724808e43cfbb782c4ad39858d24fd94baaabe50
Garrett Rapport <grrapport@gmail.com>
2019-11-26 22:39:42 -0600

    fixed query again

--- NcaabSqlHandler.py
+++ NcaabSqlHandler.py
@@ -42,7 +42,7 @@ class SqlHandler:
         self.mydb.commit()
 
     def select_latest_ncaa_line(self, gametime, bookmaker, team1, team2):
-        ret = []
+        new_line = None
         select_latest_full_game_ncaab_query = ("SELECT * FROM NCAAB_Full_Game_Lines WHERE "
                                                "(Team1 = %s or Team2 = %s) AND "
                                                "(Team1 = %s or Team2 = %s) AND "
@@ -57,20 +57,20 @@ class SqlHandler:
         for temp in result:
             new_line = GameLines.FullGameLine(temp[0], temp[1], temp[2], temp[5], temp[6], temp[7], temp[3], temp[8],
                                               temp[9], temp[10], temp[4], temp[11], temp[12], temp[13])
-            ret.append(new_line)
-        return ret
+            break
+        return new_line
 
     def select_all_available_ncaab_full_game_lines(self):
         now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        new_line = None
+        ret = []
         select_all_full_game_ncaab_query = "SELECT * FROM NCAAB_Full_Game_Lines WHERE GameTime > %s"
         self.cursor.execute(select_all_full_game_ncaab_query, (now, ))
         result = self.cursor.fetchall()
         for temp in result:
             new_line = GameLines.FullGameLine(temp[0], temp[1], temp[2], temp[5], temp[6], temp[7], temp[3], temp[8],
                                               temp[9], temp[10], temp[4], temp[11], temp[12], temp[13])
-            break
-        return new_line
+            ret.append(new_line)
+        return ret
 
     def select_all_available_ncaab_full_game_lines_by_bookmaker(self, bookmaker):
         now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
