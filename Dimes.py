db2b6005b551618bbed3e9f73a37f14eb7b38798
Garrett Rapport <grrapport@gmail.com>
2020-02-10 21:27:11 -0600

    now adding spreads for Dimes

--- Dimes.py
+++ Dimes.py
@@ -2,6 +2,8 @@ import requests
 import bs4
 import datetime
 import GameLines
+from urllib.parse import unquote
+
 
 user_agent = {
     'User-Agent': 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
@@ -69,18 +71,18 @@ def get_odds_from_row(tr):
 def get_game_lines_by_league(session, league_id):
     league_id = 'strID='+league_id
     dimes_response = str(session.post(odds_url, headers=user_agent, cookies=session.cookies, data=league_id).content, 'utf-8')
+    dimes_response = unquote(dimes_response)
     dimes_response = dimes_response.replace("\\n", "").replace("\\r", "").replace("\\t", "").replace("\'", "").replace(
-        "\\", "").replace("½", ".5")
+        "\\", "").replace("\xa0", " ")
     soup = bs4.BeautifulSoup(dimes_response, features="html.parser")
     all_tr = soup.findAll("tr", {"class": "LHR"})
     lines = []
-    current_line = None
     for tr in all_tr:
         temp_teams_date = get_teams_and_date(tr)
         # we only want lines up to a certain future time. Not sure what to make this yet
-        if temp_teams_date[2] > datetime.datetime.now() + datetime.timedelta(hours=36):
-            break
         if temp_teams_date is not None:
+            if temp_teams_date[2] > datetime.datetime.now() + datetime.timedelta(hours=36):
+                break
             temp_gt = temp_teams_date[2]
             temp_t1 = temp_teams_date[0]
             temp_t2 = temp_teams_date[1]
@@ -94,14 +96,47 @@ def get_game_lines_by_league(session, league_id):
             temp_t2_spr = None
             temp_t2_spr_line = None
 
-            if current_line is not None:
-                lines.append(current_line)
-                current_line = None
             next_row = tr.find_next("tr")
-            print(next_row)
+
+            # If a row contains either team name, is hidden  (there are a bunch hidden)
+            # or the class is LHR (which is used for titles and also blank lines, we keep going
+            while temp_t1 in str(next_row) or temp_t2 in str(next_row) or "hidden" in str(next_row) or next_row["class"][0] == "LHR":
+                if ("hidden" in str(next_row) or next_row["class"][0] == "LHR") and True: # add something to check for odds classes, just in case
+                    next_row = next_row.find_next("tr")
+                    continue
+                row_parts = next_row.findAll("td")
+
+                # next code block gets the spread from this row
+                spread_div = row_parts[1]
+                # checking to see if any rows don't have a spread at all by checking to see if the price is listed
+                # TODO: check of alternate lines by looking for prices that are not -110
+                if len(spread_div.find_next("span", {"class": "US"}).contents) > 0:
+                    spread_str = spread_div.contents[0].replace("½", ".5")
+                    print(spread_str)
+                    print(spread_div.find_next("span", {"class": "US"}).contents)
+                    spread_juice = int(spread_div.find_next("span", {"class": "US"}).contents[0])
+                    if spread_str is None or spread_str == " ":
+                        spread = None
+                    else:
+                        if "pk" in spread_str:
+                            spread = 0.0
+                        else:
+                            spread = float(spread_str)
+                    if temp_t1 in str(row_parts[0]):
+                        temp_t1_spr = spread
+                        temp_t1_spr_line = spread_juice
+                    if temp_t2 in str(row_parts[0]):
+                        temp_t2_spr = spread
+                        temp_t2_spr_line = spread_juice
+
+                next_row = next_row.find_next("tr")
+            print("\n\n\tthis row broke the loop for "+temp_t1+" and "+temp_t2)
+            print(str(next_row)+"\n\n")
             current_line = GameLines.FullGameLine(temp_gt, datetime.datetime.now(), "5Dimes", None, None,
                                                   None, temp_t1, None,
-                                                  None, None, temp_t2, None, None, None)
+                                                  temp_t1_spr, temp_t1_spr_line, temp_t2, None,
+                                                  temp_t2_spr, temp_t2_spr_line)
+            lines.append(current_line)
     return lines
 
 
@@ -122,3 +157,4 @@ def get_ncaab_full_game_lines():
 odds = get_ncaab_full_game_lines()
 for line in odds:
     print(line.output())
+
